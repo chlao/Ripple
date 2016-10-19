cc04bb0d701e939e10fe82f57cdbd65565f47bd8
Christine Lao <clao@ucsd.edu>
2016-10-18 21:40:37 -0700

    Moved creating and filling a table to /ip

--- ripple.go
+++ ripple.go
@@ -46,14 +46,6 @@ func NewDB() *sql.DB {
 	db, err := sql.Open("postgres", os.Getenv("DATABASE_URL"))
 	//db, err := sql.Open("sqlite3", "./ripple.db")
     checkErr(err)
-    
-    _, err = db.Exec("DROP TABLE IF EXISTS requests")
-    checkErr(err)
-
-    _, err = db.Exec("CREATE TABLE IF NOT EXISTS requests(request_id text primary key, timestamp text, fwd text)")
-    checkErr(err)
-
-    insertLogs(db)
 
     return db
 }
@@ -106,6 +98,14 @@ type IPAddresses struct{
 }
 
 func GetIP(db *sql.DB) http.Handler{
+	_, err = db.Exec("DROP TABLE IF EXISTS requests")
+    checkErr(err)
+
+    _, err = db.Exec("CREATE TABLE IF NOT EXISTS requests(request_id text primary key, timestamp text, fwd text)")
+    checkErr(err)
+
+    insertLogs(db)
+	
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Content-Type", "application/json")	
 
