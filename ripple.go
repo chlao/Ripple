59f11b74e12f80aa0582e985defde6060ae0dbb9
Christine Lao <clao@ucsd.edu>
2016-10-18 22:10:28 -0700

    Removed create table sql query

--- ripple.go
+++ ripple.go
@@ -46,6 +46,16 @@ func NewDB() *sql.DB {
 	db, err := sql.Open("postgres", os.Getenv("DATABASE_URL"))
 	//db, err := sql.Open("sqlite3", "./ripple.db")
     checkErr(err)
+    
+    /*
+    _, err = db.Exec("DROP TABLE IF EXISTS requests")
+    checkErr(err)
+
+    _, err = db.Exec("CREATE TABLE IF NOT EXISTS requests(request_id text primary key, timestamp text, fwd text)")
+    checkErr(err)
+
+    insertLogs(db)
+    */
 
     return db
 }
@@ -99,14 +109,6 @@ type IPAddresses struct{
 
 func GetIP(db *sql.DB) http.Handler{
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		_, err := db.Exec("DROP TABLE IF EXISTS requests")
-	    checkErr(err)
-
-	    _, err = db.Exec("CREATE TABLE IF NOT EXISTS requests(request_id text primary key, timestamp text, fwd text)")
-	    checkErr(err)
-
-	    insertLogs(db)
-
 		w.Header().Set("Content-Type", "application/json")	
 
 		ip := make([]string, 0)
